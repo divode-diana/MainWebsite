96699d3ddd7c82f2f797a67146531cbd61e794af
Diana Fonte <dianafonte@MacBook-Air-de-Diana-3.local>
2024-10-28 19:36:15 +0000

    setup scss

--- divode/src/constants/translations.tsx
+++ divode/src/constants/translations.tsx
@@ -1,11 +1,32 @@
 export const TRANSLATIONS = {
     locale: "pt",
     Menu: {
-        "services": "Serviços",
-        "about": "Sobre",
-        "contacts": "Contactos"
+        services: "Serviços",
+        about: "Sobre",
+        contacts: "Contactos"
     },
-    Homepage: {
-        "HELLO": "Helloworld!"
+    Landing: {
+        title: "Serviços Digitais",
+        by: "por",
+        author: "Diana Fonte",
+        subtitle: "Eleve o seu negócio a novos níveis!"
+    },
+    Services: {
+        Design: {
+            title: "Branding e Design UX/UI",
+            hmtl: "Melhore o alcance do seu negócio com uma <b>renovação da imagem gráfica</b> que evidencia o melhor que tem a oferecer! </br> Branding, Design de UX e Design de UI colaboram para criar marcas e produtos digitais eficientes, eficazes e agradáveis, atendendo às necessidades dos utilizadores e superando suas expectativas."
+        },
+        Development: {
+            title: "Desenvolvimento Web",
+            html: "Este serviço refere-se à <b>criação de websites</b> para a internet totalmente customizados, sem o uso de templates. </br> Envolve programação, responsividade, acessibilidade e outras funcionalidades para criar soluções interativas e funcionais para a web. "
+        },
+        Domains: {
+            title: "Domínios e Alojamento",
+            html: "<b>Coloque o seu website online</b> para que todos o possam usar! </br> Este serviço incluí a definição e compra de um domínio só seu e a colocação do website na internet."
+        },
+        Management: {
+            title: "Gestão e Manutenção",
+            html: " Manter um website atualizado é crucial para garantir que ele funcione com capacidade total e atraia e retenha visitantes. Por esta razão facultamos este serviço, para que possa facilmente <b>manter o seu negócio sempre atualizado</b>."
+        },
     }
 }
