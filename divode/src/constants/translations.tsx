21eb3211b00cad6ab96df187c7e65f251010eeed
Diana Fonte <dianafonte@Air-de-Diana-3.lan>
2025-03-22 22:29:04 +0000

    english translations

--- divode/src/constants/translations.tsx
+++ divode/src/constants/translations.tsx
@@ -71,7 +71,7 @@ export const TRANSLATIONS = [
             author: "Diana Fonte",
             subtitle: "Lift your business to new levels!"
         },
-        Services: {
+        services: {
             design: {
                 title: "Branding and UX/UI Design",
                 hmtl: "Improve your business with a <b>makeover</b> that brings out the best you have to offer! </br> Branding, UX Design and UI Design work together to create brands and digital products that are eficient, pleasant, fulfil the users's needs and surpass your expectations."
@@ -82,40 +82,40 @@ export const TRANSLATIONS = [
             },
             domains: {
                 title: "Domains and Hosting",
-                html: "<b>Coloque o seu website online</b> para que todos o possam usar! </br> Este serviço incluí a definição e compra de um domínio só seu e a colocação do website na internet."
+                html: "<b>Put your website online</b> so everyone can use it!</br> This service includes defining and purchasing your own domain and putting the website on the internet."
             },
             management: {
-                title: "Gestão e Manutenção",
-                html: " Manter um website atualizado é crucial para garantir que ele funcione com capacidade total e atraia e retenha visitantes. Por esta razão facultamos este serviço, para que possa facilmente <b>manter o seu negócio sempre atualizado</b>."
+                title: "Management and Maintenance",
+                html: "Keeping a website up to date is crucial to ensure it operates at full capacity and attracts and retains visitors. For this reason, we offer this service so you can <b>easily keep your business always up to date</b>."
             },
         },
         about: {
-            p1: "A <b>Divode é uma consultora unipessoal</b> criada pelas necessidades de um mundo virtual cada vez exigente e competitivo.",
-            p2: "Eu, <a href={'https://www.linkedin.com/in/dianafonte/'} target='_blank'>Diana Fonte</a>, como fundadora e única associada, companhar-vos-ei durante todo o processo, desde a conceção à realização dos vossos sonhos.",
-            p3: "Como <b>desenvolvedora e designer web</b>, encontro-me na interseção entre criatividade, tecnologia e experiência do utilizador.",
-            p4: "Além dos pixels e do código, desejo contribuir positivamente. Quer seja através de designs acessíveis, práticas ecológicas ou projetos conduzidos pela comunidade, acredito em usar as minhas competências para criar um mundo digital melhor."
+            p1: "<b>Divode is a sole consultancy</b> created to meet the needs of an increasingly demanding and competitive virtual world.",
+            p2: "I, <a href={'https://www.linkedin.com/in/dianafonte/'} target='_blank'>Diana Fonte</a>, as the founder and sole partner, will accompany you throughout the entire process, from conception to the realization of your dreams.",
+            p3: "As a <b>web developer and designer</b>, I find myself at the intersection of creativity, technology, and user experience.",
+            p4: "Beyond pixels and code, I wish to contribute positively. Whether through accessible designs, eco-friendly practices, or community-driven projects, I believe in using my skills to create a better digital world."
         },
         contacts:{
-            title: "Contacte-nos!",
-            subtitle: "Quer trabalhar connosco ou tem alguma dúvida?",
+            title: "Contact us!",
+            subtitle: "You want to work with us or you have a question?",
             form:{
-                mandatory: "Obrigatório",
-                label1: "Nome",
-                placeholder1: "Nome",
-                label2: "Telefone",
+                mandatory: "Mandatory",
+                label1: "Name",
+                placeholder1: "Name",
+                label2: "Phone",
                 placeholder2: "+351910000000",
                 label3: "Email",
-                placeholder3: "nome.apelido@exemplo.com",
-                label4: "Mensagem",
-                placeholder4: "Escreva aqui a sua mensagem.",
-                submit: "Enviar",
-                success: "Mensagem enviada com sucesso! Entraremos em contacto consigo brevemente.",
-                error: "Não foi possível enviar a mensagem! Por favor tente outra vez mais tarde ou envie-nos um email directamente. Pedimos desculpa pelo incómodo."
+                placeholder3: "name@example.com",
+                label4: "Message",
+                placeholder4: "Write your message here.",
+                submit: "Send",
+                success: "Message sent with success! We will contact you soon.",
+                error: "There was an error sending your message! Please try again or send us an email to our email address. We are sorry for the incovenience."
             }
         },
         footer: {
-            copyright: "Copyright. Todos os direitos reservados.",
-            subtitle: "Divode Serviços Digitais",
+            copyright: "Copyright. All rights reserved.",
+            subtitle: "Divode Digital Services",
             email: "divodedigitalservices@gmail.com"
         }
     }
