f6ebb88847a3a29731702930214e6b513bbfe4de
divode-diana <diana@divode.io>
2025-06-02 23:06:51 +0100

    termos e condicoes page

--- divode/src/pages/Termos.tsx
+++ divode/src/pages/Termos.tsx
@@ -1,5 +1,28 @@
+import { Link } from "react-router-dom";
+import { TRANSLATIONS } from "../constants/translations";
+import { useLanguage } from "../context/LanguageContext";
+import Icon from "../components/Icon";
+
 const Termos = () => {
-    return <>Termos</>
+    const { language } = useLanguage();
+    const content = TRANSLATIONS[language];
+
+    return (
+        <section className="mt-10 d-flex flex-column gap-4 maxw-md-80 mx-auto">
+            <Link to={"/"} className="d-flex gap-1 fs-6 align-items-baseline text-decoration-none">
+                <Icon icon={"chevron-left"} /> <span className="text-decoration-underline">{content.rgpd_terms.back}</span>
+            </Link>
+
+            <div className="d-flex flex-column gap-4 ">
+                <h2>{content.rgpd_terms.terms?.title}</h2>
+                <p
+                    dangerouslySetInnerHTML={{
+                        __html: content.rgpd_terms.terms.texto,
+                    }}
+                />
+            </div>
+        </section>
+    );
 }
 
 export default Termos
